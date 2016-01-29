2c90ca638a8c28bd8fc406dd4f72ebfb54c032bc
Chad Estep <chadestep@users.noreply.github.com>
2016-01-28 20:06:05 -0600

    Titipat's __init__.py

---
+++ neurphys/__init__.py
@@ -0,0 +1,17 @@
+"""
+Neurphys ('nervous')
+--------------------
+IO and analysis package built to streamline and standardize
+the data handling, analysis, and visualization of electrophysiology
+and calcium imaging data
+"""
+
+__version__ = '0.0'
+
+from .calcium import calc_ca_conc
+from .membrane import calc_mem_prop
+from .oscillation import create_epoch, epoch_hist, epoch_kde, epoch_pgram
+from .pv_import import parse_xml, import_vr_csv, import_ls_csv, import_folder
+from .read_abf import read_abf
+from .synaptics import analyze_current, calc_ppr
+from .utilities import baseline, find_peak, calc_decay, simple_smoothing
