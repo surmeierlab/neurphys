6a73068f27800c447b18d0ba2a546dc8d1bd2fa9
Chad Estep <chadestep@gmail.com>
2016-02-01 12:20:20 -0600

    importing issue

--- neurphys/__init__.py
+++ neurphys/__init__.py
@@ -13,5 +13,5 @@ from .membrane import calc_mem_prop
 from .oscillation import create_epoch, epoch_hist, epoch_kde, epoch_pgram
 from .pv_import import parse_xml, import_vr_csv, import_ls_csv, import_folder
 from .read_abf import read_abf, keep_sweeps, drop_sweeps
-from .synaptics import analyze_current, calc_ppr
 from .utilities import baseline, find_peak, calc_decay, simple_smoothing
+from .synaptics import analyze_current, calc_ppr
