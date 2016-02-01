2d985dd7a84dc90f5ea404f10a7d6631ed0431a4
Chad Estep <chadestep@gmail.com>
2016-02-01 12:11:52 -0600

    added keep/drop sweep functions to read_abf

--- neurphys/__init__.py
+++ neurphys/__init__.py
@@ -12,6 +12,6 @@ from .calcium import calc_ca_conc
 from .membrane import calc_mem_prop
 from .oscillation import create_epoch, epoch_hist, epoch_kde, epoch_pgram
 from .pv_import import parse_xml, import_vr_csv, import_ls_csv, import_folder
-from .read_abf import read_abf
+from .read_abf import read_abf, keep_sweeps, drop_sweeps
 from .synaptics import analyze_current, calc_ppr
 from .utilities import baseline, find_peak, calc_decay, simple_smoothing
--- neurphys/read_abf.py
+++ neurphys/read_abf.py
@@ -4,7 +4,7 @@ __author__ = "Dan Galtieri and Chad Estep (chadestep@gmail.com)"
 from neo import io
 import pandas as pd
 
-def read_abf(filename):
+def read_abf(filepath):
     """
     Imports ABF file using neo io AxonIO, breaks it down by blocks
     which are then processed into a multidimensional pandas dataframe
@@ -14,19 +14,19 @@ def read_abf(filename):
     Parameters
     ----------
     filename: str
-        filename WITH '.abf' extension
+        Full filepath WITH '.abf' extension.
 
-    Returns
-    ------_
+    Return
+    ------
     df: DataFrame
-        Pandas DataFrame broken down by sweep
+        Pandas DataFrame broken down by sweep.
 
     References
     ----------
     [1] https://neo.readthedocs.org/en/latest/index.html
     """
 
-    r = io.AxonIO(filename = filename)
+    r = io.AxonIO(filename=filepath)
     bl = r.read_block(lazy=False, cascade=True)
     num_channels = len(bl.segments[0].analogsignals)
     channels = ['primary']
@@ -38,12 +38,75 @@ def read_abf(filename):
         for i in range(num_channels):
             signals.append(bl.segments[seg_num].analogsignals[i])
             if i >= 1:
-                channels.append('channel_' + str(i))
+                channels.append('channel_'+str(i))
         data_dict = dict(zip(channels, signals))
         time = seg.analogsignals[0].times - seg.analogsignals[0].times[0]
         data_dict['time'] = time
         df = pd.DataFrame(data_dict)
         df_list.append(df)
-        sweep_list.append('sweep' + str(seg_num + 1).zfill(3))
+        sweep_list.append('sweep'+str(seg_num+1).zfill(3))
     df = pd.concat(df_list, keys=sweep_list, names=['sweep'])
     return df
+
+def keep_sweeps(df, sweep_list):
+    """
+    Keeps specified sweeps from your DataFrame.
+
+    Parameters
+    ----------
+    df: Pandas DataFrame
+        Dataframe created using one of the functions from Neurphys.
+    sweep_list: 1D array_like of ints
+        List containing numbers of the sweeps you'd like dropped from the DataFrame.
+
+    Return
+    ------
+    keep_df: Pandas Dataframe
+        Dataframe containing only the sweeps you want to keep.
+
+    Notes
+    -----
+    Making the grand assumption that the df.index.level[0]=='sweeps'.
+
+    TODO
+    ----
+    As always, I need to build exceptions into this function and just need to make it generally more robust.
+    - make possibility for 'sweep_list' to be either pure numerical list, or actually contain list of sweep names (ex. ['sweep002','sweep016'])
+    """
+
+    keep_sweeps = [('sweep'+str(i).zfill(3)) for i in sweep_list]
+    keep_df = df.loc[pd.IndexSlice[keep_sweeps]]
+    return keep_df
+
+def drop_sweeps(df, sweep_list):
+    """
+    Removes specified sweeps from your DataFrame.
+
+    Parameters
+    ----------
+    df: Pandas DataFrame
+        Dataframe created using one of the functions from Neurphys.
+    sweep_list: 1D array_like of ints
+        List containing numbers of the sweeps you'd like dropped from the DataFrame.
+
+    Return
+    ------
+    drop_df: Pandas Dataframe
+        Dataframe containing only the sweeps you want to keep.
+
+    Notes
+    -----
+    Making the grand assumption that the df.index.level[0]=='sweeps'.
+
+    TODO
+    ----
+    As always, I need to build exceptions into this function and just need to make it generally more robust.
+    - make possibility for 'sweep_list' to be either pure numerical list, or actually contain list of sweep names (ex. ['sweep002','sweep016'])
+    """
+
+    all_sweeps = df.index.levels[0].values
+    drop_sweeps = [('sweep'+str(i).zfill(3)) for i in sweep_list]
+    # keep_sweeps = list(set(all_sweeps).difference(drop_sweeps))
+    keep_sweeps = list(set(all_sweeps) ^ set(drop_sweeps))
+    drop_df = df.loc[pd.IndexSlice[keep_sweeps]]
+    return drop_df
