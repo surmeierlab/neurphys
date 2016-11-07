4b57b65e0570ce5c2720a5352a513126f996f404
Chad Estep <chadestep@gmail.com>
2016-11-07 13:00:17 -0600

    updated init with new oscillations

--- neurphys/__init__.py
+++ neurphys/__init__.py
@@ -12,7 +12,7 @@ from .utilities import baseline, find_peak, calc_decay, simple_smoothing
 from .calcium import calc_ca_conc
 from .membrane import calc_mem_prop
 from .nuplot import simple_axis, simple_figure, clean_axis, clean_figure, nu_legend, nu_boxplot, nu_scatter, nu_raster, nu_violin
-from .oscillation import create_epoch, epoch_hist, epoch_kde, epoch_pgram
+from .oscillation import epoch_hist, epoch_kde, epoch_pgram
 from .pacemaking import detect_peaks, baseline_pacemaking, calc_freq, iei_arrays
 from .read_abf import read_abf, keep_sweeps, drop_sweeps
 from .read_pv import parse_xml, import_vr_csv, import_ls_csv, import_folder
