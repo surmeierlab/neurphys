964b9339cc7a1a73cd4e9de490a5045d570066f4
Chad Estep <chadestep@gmail.com>
2017-04-28 13:16:36 -0500

    Testing and docs: update on current problems

--- tests/test_read_abf.py
+++ tests/test_read_abf.py
@@ -1,4 +1,5 @@
 import neurphys.read_abf as read_abf
+import neurphys.utilities as util
 
 
 def test_all_strs():
@@ -14,8 +15,19 @@ def test_all_ints():
     assert read_abf._all_ints(['test', '12345'])  == False
     assert read_abf._all_ints(['test', 12345]) == False
     assert read_abf._all_ints([54321, 12345])
-    assert read_abf._all_ints([43.21, 12.34]) == False
+    assert read_abf._all_ints(['4321', 12.34]) == False
     assert read_abf._all_ints(('test', '12345'))  == False
     assert read_abf._all_ints(('test', 12345)) == False
     assert read_abf._all_ints((54321, 12345))
-    assert read_abf._all_ints((43.21, 12.34)) == False
+    assert read_abf._all_ints(('4321', 12.34)) == False
+
+# Testing of keep/drop functions is on hold until bugs have been worked out
+
+# def test_keep_sweeps():
+#     df = util.mock_multidf()
+#     sweep_ints  = [1,4]
+#     sweep_mixed = [1,4,5.23]
+#     sweep_names = ['sweep002','sweep003']
+#     sweep_wrong = ['sweepnop']
+#     sweep_strnotsweep = ['thing']
+#     sweep_disoredered = ['sweep003', 'sweep002']
