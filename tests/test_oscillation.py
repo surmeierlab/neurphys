9e82be48b64e31e145a6cf1052b3ca85bfa4ac34
Chad Estep <chadestep@gmail.com>
2017-05-02 21:19:56 -0500

    minor aesthetic changes

--- tests/test_oscillation.py
+++ tests/test_oscillation.py
@@ -1,6 +1,12 @@
 import neurphys.utilities as util
-import neurphys.oscillation as oscillation
+import neurphys.oscillation as oscil
 
 
 def test_create_epoch():
+    """placeholder"""
+    pass
+
+
+def test_epoch_attr():
+    """placeholder"""
     pass
